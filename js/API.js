a34a6791d271e498eedfb676b50acbdcd75bc48e
Ruzgar Ata Ozkan <ruzgarata6@gmail.com>
2020-05-07 20:42:35 +0300

    casual API superClass added

--- js/API.js
+++ js/API.js
@@ -4,4 +4,15 @@ class API {
     constructor() {
 
     }
+
+    getAPI(url) {
+        $.get(url, () => {
+            console.log('Fetching url');
+        }).done((data) => {
+            console.log('Fetching url done.');
+            console.log(data);
+        }).fail(() => {
+            console.log('Could not fetch the specified url.');
+        });
+    }
 }
