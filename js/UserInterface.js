89a2f167abcd8b66f6e1a9de56631c986280b0a4
Ruzgar Ata Ozkan <ruzgarata6@gmail.com>
2020-05-07 20:37:36 +0300

    header scroll behavior added

--- js/UserInterface.js
+++ js/UserInterface.js
@@ -37,16 +37,35 @@ class UserInterface {
                 behavior: 'smooth'
             });
         });
+
+        $(window).scroll(() => {
+            const pageY = window.pageYOffset;
+            if (pageY > 30) {
+                $('header').addClass('header-scroll');
+                $('.global-nav').find('a').css('color', 'black');
+            } else if (pageY <= 30) {
+                if (window.innerWidth >= 992) {
+                    $('header').removeClass('header-scroll');
+                    $('.global-nav').find('a').css('color', 'white');
+                }
+            }
+        });
     }
 
     // update the naviagtion bar when the sceen resized change all the children none or block or inline-block
     static updateNavBar(switcher) {
         $(window).resize(() => {
             if (window.innerWidth >= 992) {
+                if (window.pageYOffset < 30) {
+                    $('header').removeClass('header-scroll');
+                    $('.global-nav').find('a').css('color', 'white');
+                }
                 $('.dropdown').css('display', 'inline-block');
                 $('.dropdown').children().css('display', 'inline-block');
             }
             else if (window.innerWidth < 991) {
+                $('.global-nav').find('a').css('color', 'black');
+                $('.dropdown').children().css('color', 'black');
                 switch (switcher) {
                     case true:
                         $('.dropdown').css('display', 'none');
